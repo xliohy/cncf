823302d9f79b519da496985ed907318788e24c7b
gmstcl <as.gmstcl@gmail.com>
2025-02-24 17:11:11 +0900

    init

--- Continuous Integration & Delivery/argocd/install.sh
+++ Continuous Integration & Delivery/argocd/install.sh
@@ -10,8 +10,8 @@ $ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"
 # ============================================================
 $ ARGOCD_VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
 $ curl -sSL -o /tmp/argocd-${ARGOCD_VERSION} https://github.com/argoproj/argo-cd/releases/download/${ARGOCD_VERSION}/argocd-linux-amd64
-$ chmod +x /tmp/argocd-${VERSION}
-$ sudo mv /tmp/argocd-${VERSION} /usr/local/bin/argocd 
+$ chmod +x /tmp/argocd-${ARGOCD_VERSION}
+$ sudo mv /tmp/argocd-${ARGOCD_VERSION} /usr/local/bin/argocd 
 $ argocd version --client
 
 # ============================================================
