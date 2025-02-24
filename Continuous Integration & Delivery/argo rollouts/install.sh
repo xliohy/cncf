d3967a99f0d600e2315b522b9e88a67d55171f6c
gmstcl <as.gmstcl@gmail.com>
2025-02-24 15:09:00 +0900

    init

--- Continuous Integration & Delivery/argo rollouts/install.sh
+++ Continuous Integration & Delivery/argo rollouts/install.sh
@@ -3,7 +3,9 @@
 # ============================================================
 $ kubectl create ns argo-rollouts
 $ helm repo add argo https://argoproj.github.io/argo-helm
-$ helm install argo-rollouts argo/argo-rollouts -n argo-rollouts --set dashboard.enabled=true --set dashboard.image.tag=v1.7.2
+$ helm install argo-rollouts argo/argo-rollouts -n argo-rollouts --set dashboard.enabled=true --set dashboard.image.tag=v1.7.2 
+    # 만약 재 생성을 해야하는 경우에는 argoproj crds를 삭제 후에 다시 helm install을 해야합니다. 아래 명령어 참고.
+    # k delete crd  applicationsets.argoproj.io  applications.argoproj.io appprojects.argoproj.io
 
 # ------------> https://quay.io/repository/argoproj/kubectl-argo-rollouts?tab=tags&tag=latest 
 # bashboard Tags를 수정해야 합니다. latest 버전은 현재 접근이 불가한 상태로 v1.7.2 버전을 사용했습니다. 
